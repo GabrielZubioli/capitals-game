628c72f48b595ddb8d8c03abdbc1d30f18028bfe
Gabriel Zubioli <zubioligabriel@gmail.com>
2025-04-27 00:47:28 -0300

    end game

--- public/index.php
+++ public/index.php
@@ -1,26 +1,23 @@
 <?php
+
 session_start();
 
 $route = $_GET['route'] ?? 'start';
 
 $script = null;
-
 switch ($route) {
     case 'start':
-        $script = 'start'; 
+        $script = 'start';
         break;
-
-    case 'game';
+    case 'game':
         $script = 'game';
         break;
-
     case 'gameover':
         $script = 'gameover';
         break;
-
     default:
-    $script = '404';
-    break;
+        $script = '404';
+        break;
 }
 
 $capitals = require __DIR__ . '/../data/capitals.php';
@@ -28,5 +25,3 @@ $capitals = require __DIR__ . '/../data/capitals.php';
 require_once __DIR__ . "/../scripts/header.php";
 require_once __DIR__ . "/../scripts/$script.php";
 require_once __DIR__ . "/../scripts/footer.php";
-
-?>
--- scripts/start.php
+++ scripts/start.php
@@ -1,51 +1,56 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] =='POST'){
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
     $total_questions = intval($_POST['text_total_questions']) ?? 10;
 
     prepare_game($total_questions);
-    header(header: 'Location: index.php?route=game');
+
+    header('Location: index.php?route=game');
     exit;
 }
-function prepare_game($total_questions){
 
-        global $capitals;
-        $ids = [];
-        while(count($ids) < $total_questions){
-            $id= rand(0, count($capitals) -1);
-            if(!in_array($id, $ids)){
-                $ids[] = $id;
-            }  
+function prepare_game($total_questions)
+{
+    global $capitals;
+
+    $ids = [];
+    while(count($ids) < $total_questions) {
+        $id = rand(0, count($capitals) - 1);
+        if(!in_array($id, $ids)) {
+            $ids[] = $id;
         }
+    }
 
-        $question = [];
-        foreach($ids as $id){
-            $answres = [];
-            $answres[] = $id;
-            while(count($answres) < 3){
-                $tmp = rand(0, count($capitals) -1);
-                if(!in_array($tmp, $answres)){
-                    $answres[] = $tmp;
+    $questions = [];
+    foreach($ids as $id) {
+        
+        $answers = [];
+        $answers[] = $id;
+        while(count($answers) < 3) {
+            $tmp = rand(0, count($capitals) - 1);
+            if(!in_array($tmp, $answers)) {
+                $answers[] = $tmp;
             }
         }
 
-        shuffle($answres);
+        shuffle($answers);
 
-        $question[] = [
-            'question'=> $capitals[0],
-            'correct_answer'=> $id,
-            'answers'=> $answres
+        $questions[] = [
+            'question' => $capitals[$id][0],
+            'correct_answer' => $id,   
+            'answers' => $answers 
         ];
+
     }
 
-    $_SESSION['questions'] = $question;
+    $_SESSION['questions'] = $questions;
 
     $_SESSION['game'] = [
         'total_questions' => $total_questions,
         'current_question' => 0,
         'correct_answers' => 0,
-        'incorrect_qanswers' => 0,
-
-
+        'incorrect_answers' => 0,
     ];
 }
 
@@ -55,18 +60,20 @@ function prepare_game($total_questions){
     <div class="row justify-content-center">
         <div class="col-6">
             <div class="card p-5">
+
                 <div class="row">
                     <div class="col text-center">
                         <h3>Jogo das Capitais</h3>
                         <hr>
                     </div>
                 </div>
+
                 <div class="row justify-content-center">
                     <div class="col-4">
                         <form action="index.php?route=start" method="post">
                             <div class="mb-3">
-                                <label for="text_total_questions">Numero de questões:</label>
-                                <input type="number" name="text_total_questions" class="form-control form-control-lg text-center" min="3" max="20" value="10">
+                                <label for="text_total_questions" class="form-label">Número de questões:</label>
+                                <input type="number" class="form-control form-control-lg text-center" id="text_total_questions" name="text_total_questions" value="10" min="1" max="20">
                             </div>
                             <div class="text-center">
                                 <button type="submit" class="btn btn-success w-100">Iniciar</button>
@@ -74,6 +81,7 @@ function prepare_game($total_questions){
                         </form>
                     </div>
                 </div>
+
             </div>
         </div>
     </div>
