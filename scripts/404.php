044a23f925324399b7a5c3b2f3fd042a5ed34904
Gabriel Zubioli <zubioligabriel@gmail.com>
2025-04-26 23:58:31 -0300

    create structure project

---
+++ scripts/404.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>404 - Página não encontrada</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-6 text-center alert alert-danger p-5">
+                <h4 class="mb-5">404 - Página não encontrada</h4>
+                <a href="index.php?route=start" class="btn btn-danger w-25">Voltar</a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ scripts/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jogo da Capitais</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
+
+</head>
+<body class="bg-dark text-white">
+    
+</body>
+</html>
